002182e7dc5d4a32d7594c2d96a14cb17751c0c6
OvchenkoSergiy <ovchenkosergiy@gmail.com>
2020-05-04 16:45:28 +0300

    added headles to config

--- src/test/java/com/softserveinc/atqc/tests/BaseTest.java
+++ src/test/java/com/softserveinc/atqc/tests/BaseTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import static com.codeborne.selenide.Configuration.startMaximized;
+import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.clearBrowserCookies;
 
 public class BaseTest {
@@ -17,6 +17,7 @@ public class BaseTest {
     @BeforeClass
     public void setUp() {
         clearBrowserCookies();
+        headless = true;
         startMaximized = true;
 
         try {
